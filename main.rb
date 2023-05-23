c94a541c5a5e24c026a5e82789cf4b69ebc32380
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-05-23 19:47:08 +0530

    Add book , rental and classroom classes

--- main.rb
+++ main.rb
@@ -3,7 +3,9 @@ require_relative 'class/student'
 require_relative 'class/teacher'
 require_relative 'class/capitalize_decorator'
 require_relative 'class/trimmer_decorator'
+require_relative 'class/classroom'
 
+# Create a Person object
 person = Person.new(22, name: 'maximilianus')
 puts "Person Name: #{person.correct_name}"
 
@@ -13,19 +15,11 @@ puts "Capitalized Person Name: #{capitalized_person.correct_name}"
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts "Capitalized and Trimmed Person Name: #{capitalized_trimmed_person.correct_name}"
 
-# Create a Person object
-person = Person.new(25)
-puts "Person ID: #{person.id}"
-puts "Person Name: #{person.name}"
-puts "Person Age: #{person.age}"
-puts "Can Use Services? #{person.can_use_services?}"
-person.name = 'John'
-person.age = 30
-puts "Person Name: #{person.name}"
-puts "Person Age: #{person.age}"
+# Create a Classroom object
+classroom = Classroom.new('Classroom A')
 
-# Create a Student object
-student = Student.new(16, 'Classroom A')
+# Create a Student object and add it to the classroom
+student = Student.new(16, classroom)
 puts "Student ID: #{student.id}"
 puts "Student Name: #{student.name}"
 puts "Student Age: #{student.age}"
